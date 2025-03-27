4bcf2b657e3eb04f7a155861b50cbfe711594bd2
brewbon <buzzjoghee@gmail.com>
2025-03-28 01:16:27 +0530

    Fix type errors and update UI components

--- src/app/account/complete-registration/[token]/page.tsx
+++
@@ -1,8 +0,0 @@
-import CompleteRegistrationClient from './client';
-
-// This is a Server Component that receives the params
-export default function CompleteRegistrationPage({ params }: { params: { token: string } }) {
-  return (
-    <CompleteRegistrationClient token={params.token} />
-  );
-}
